044b3418888f143991dec1fa7cca3a1c4039f363
hiromoon <hiromoon0428@gmail.com>
2023-04-08 00:24:37 +0900

    Add webcamera view

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {useCallback, useRef} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleStart = useCallback(async () => {
+    try {
+      if (!videoRef.current) {
+        return;
+      }
+      videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      })
+      videoRef.current.play()
+    } catch (err) {
+      console.error(err)
+    }
+  }, [])
+
+  const handleStop = useCallback(() => {
+    if (!videoRef.current || !videoRef.current?.srcObject) {
+      return;
+    }
+    // @ts-ignore
+    const tracks = videoRef.current.srcObject.getTracks();
+    tracks.forEach((track:MediaStreamTrack) => track.stop())
+    videoRef.current.srcObject = null
+  }, [])
 
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <video ref={videoRef} />
     </div>
   )
 }
